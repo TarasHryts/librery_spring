e236e9e54ebf6b4737efc9853dda441afcddcd32
Тарас <tarashryts@gmail.com>
2019-10-16 16:32:17 +0300

    first commit

---
+++ src/main/java/mate/academy/spring/dao/UserDao.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.dao;
+
+import java.util.List;
+import mate.academy.spring.entity.User;
+
+public interface UserDao {
+    void add(User user);
+
+    List<User> listUsers();
+}
---
+++ src/main/java/mate/academy/spring/service/UserService.java
@@ -0,0 +1,10 @@
+package mate.academy.spring.service;
+
+import java.util.List;
+import mate.academy.spring.entity.User;
+
+public interface UserService {
+    void add(User user);
+
+    List<User> listUsers();
+}
